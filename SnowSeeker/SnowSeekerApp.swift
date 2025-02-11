464f7232d4a8736d48e6ee605d37c34707e7e00d
Saydulayev <saydulayev.wien@gmail.com>
2025-02-11 08:54:45 +0100

    Initial Commit

---
+++ SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+//  Created by Saydulayev on 11.02.25.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
